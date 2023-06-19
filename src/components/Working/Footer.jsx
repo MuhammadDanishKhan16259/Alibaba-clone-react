518db2e49cff09ff24e38b3e1d52f0a011db233f
MuhammadDanishKhan16259 <muhammaddanishkhan1999@gmail.com>
2023-06-19 19:33:44 +0500

    first commit

---
+++ src/components/Working/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+// css
+import "../../style/footer.scss";
+
+function Footer() {
+  return (
+    <React.StrictMode>
+      <section className="footer">
+        <h1>© 1999-2022 Alibaba.com. All rights reserved.</h1>
+        <p>create by Muhammad Danish Khan</p>
+
+        <a href="#">
+          <i className="fa-solid fa-arrow-up"></i>
+        </a>
+      </section>
+    </React.StrictMode>
+  );
+}
+
+export default Footer;
